74b342a6e7d031576e1a59bdf42d811c5bb02128
Tinashe Musonza <tinashemusonza@gmail.com>
2022-11-10 20:32:13 -0500

    revert

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -29,23 +29,7 @@ module.exports = {
             '/guide/': getGuideSidebar(),
             '/': getGuideSidebar()
         }
-    },
-    head: [
-        [
-            'script',
-            {
-                async: true,
-                src: 'https://www.googletagmanager.com/gtag/js?id=G-7P23Q59LRC',
-            },
-        ],
-        [
-            'script',
-            {},
-            [
-                "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-7P23Q59LRC');",
-            ],
-        ],
-      ],
+    }
 }
 
 function getGuideSidebar() {
